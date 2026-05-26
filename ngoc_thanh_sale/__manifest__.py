0034161f66d09378bd1540c4b268a4f3a999af2c
Lewis <luanpham.31231022547@st.ueh.edu.vn>
2026-05-26 12:35:58 +0700

    Thêm tính năng tính tiền cọc và thanh toán cho app Sales.

---
+++ ngoc_thanh_sale/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'ngoc_thanh_sale',
+    'version': '19.0.1.0.0',
+    'category': 'Sales',
+    'installable': True,
+    'application': True,  # Thêm đúng dòng này vào nhé
+    'depends': ['sale'],
+    'author': 'Ngoc Thanh',
+    'description': 'Custom Sale Module for Ngoc Thanh',
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+}
